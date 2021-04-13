980e8d3227b65354a71b3904130fb4363b0e6f1b
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-04-13 01:38:06 -0400

    additional req

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const expressSession = require("express-session");
 const passport = require("passport");
 const Auth0Strategy = require("passport-auth0");
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 
 require("dotenv").config();
 
@@ -59,6 +61,8 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(compression());
+app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
