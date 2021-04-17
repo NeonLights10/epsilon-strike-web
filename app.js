fac98ce4c870464e9b79bf00771c13a006f13dfc
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-04-17 16:33:49 -0400

    Fix CSP

--- app.js
+++ app.js
@@ -62,7 +62,16 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(compression());
-app.use(helmet());
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                scriptSrc: ["'self'", "https://code.jquery.com"]
+            }
+        },
+    })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
