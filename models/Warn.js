19d283cd497b69fc2187214ad92287f6a39d8023
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-04-17 17:51:44 -0400

    add long datatype mongoose

--- models/Warn.js
+++ models/Warn.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+require('mongoose-long')(mongoose);
+const {Types: {Long}} = mongoose;
 
 const warnSchema = new mongoose.Schema({
 	_id: mongoose.ObjectId,
 	time: Date,
 	server_id: Number,
 	user_name: String,
-	user_id: Number,
+	user_id: Long,
 	moderator: String,
 	message_link: String,
 	reason: String
