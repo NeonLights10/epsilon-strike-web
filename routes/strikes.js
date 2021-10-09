71c8ab2e572c0e367df72341fe7199594bc7c583
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-10-09 16:40:56 -0400

    test pagination

--- routes/strikes.js
+++ routes/strikes.js
@@ -24,17 +24,37 @@ router.get('/', secured, (req, res, next) => {
 
 /* GET users listing. */
 router.get('/strikelist', secured, (req, res, next) => {
-  Warn.find({},{},function(e,docs){
-  	res.json(docs)
-  });
+  function paginatedResults() {
+    return async (req, res, next) => {
+      
+      const page = parseInt(req.query.page);
+      const limit = parseInt(req.query.limit);
+      const skipIndex = (page - 1) * limit;
+      const results = {};
+
+      try {
+        results.results = await Warn.find()
+          .sort({ time: -1 })
+          .limit(limit)
+          .skip(skipIndex)
+          .exec();
+        res.paginatedResults = results;
+        next();
+      } catch (e) {
+        res.status(500).json({ message: "Error Occured" });
+      }
+    };
+  }
+  paginatedResults()
+  res.json(res.paginatedResults)
 });
 
 /* DELETE to deletestrike */
 router.delete('/deletestrike/:id', secured, (req, res, next) => {
-	var strikeToDelete = req.params.id;
-	Warn.deleteOne({ '_id' : strikeToDelete }, function(err) {
-		res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
-	});
+  var strikeToDelete = req.params.id;
+  Warn.deleteOne({ '_id' : strikeToDelete }, function(err) {
+    res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+  });
 });
 
 module.exports = router;
