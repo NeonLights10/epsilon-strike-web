9d5cd68fc28ea56faa4ddf6479b13f635322c754
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2021-11-05 22:02:43 -0400

    fix js config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    width: {
-      '1of2': '50%',
-    },
-    margin: {
-      '0p5': '0.125rem',
-      '2p5': '0.625rem',
-    },
     extend: {
+      width: {
+        '1of2': '50%',
+      },
+      margin: {
+        '0p5': '0.125rem',
+        '2p5': '0.625rem',
+      },
       backgroundImage: {
         'home-bg': "url('/images/bg.jpg')",
       },
